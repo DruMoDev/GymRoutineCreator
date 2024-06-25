639f76e77cec2dd1cc1de958557e97ab487ddaee
DruMoDev <dev.morera@gmail.com>
2024-06-25 15:17:16 +0200

    index

--- Backend/index.js
+++ Backend/index.js
@@ -14,14 +14,14 @@ app.use(express.json());
 connectDB();
 
 // CORS
-app.use(cors(corsOptions));
-// app.use(cors(
-//   {
-//     origin: ["https://gym-routine-creator.vercel.app"],
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     credentials: true,
-//   }
-// ));
+// app.use(cors(corsOptions));
+app.use(cors(
+  {
+    origin: ["https://gym-routine-creator.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  }
+));
 
 // Routes
 app.use("/api/user", userRoutes);
