70463b986a6a85743551959d510499ba1dd3fb0b
DruMoDev <dev.morera@gmail.com>
2024-06-25 15:14:41 +0200

    cambio de index

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+import "./src/config/dotenv.js";
+import express from "express";
+import cors from "cors";
+import connectDB from "./src/config/connectDB.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import corsOptions from "./src/config/cors.js";
+import organizationRoutes from "./src/routes/organizationRoutes.js";
+
+// Express app instance
+const app = express();
+app.use(express.json());
+
+// Connect to DB
+connectDB();
+
+// CORS
+app.use(cors(corsOptions));
+// app.use(cors(
+//   {
+//     origin: ["https://gym-routine-creator.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     credentials: true,
+//   }
+// ));
+
+// Routes
+app.use("/api/user", userRoutes);
+app.use("/api/organization", organizationRoutes);
+
+// Database connection
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
