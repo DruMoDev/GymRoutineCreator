59705f835511fe2ee3808cdbfcb3a112f3c34411
DruMoDev <dev.morera@gmail.com>
2024-06-26 12:55:42 +0200

    env quitadas

--- Frontend/src/config/clienteAxios.jsx
+++ Frontend/src/config/clienteAxios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  baseURL: `https://gym-routine-creator-backend.vercel.app/api`,
+  // baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
 });
 
 export default clienteAxios;
