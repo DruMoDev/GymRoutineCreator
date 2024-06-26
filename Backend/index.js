06f3d9f82b546b0f576da5c862145f328b8732ba
DruMoDev <dev.morera@gmail.com>
2024-06-26 12:49:01 +0200

    mejoras

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,39 @@
-import "./config/dotenv.js";
+// import "./config/dotenv.js";
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/connectDB.js";
 import userRoutes from "./routes/userRoutes.js";
 import organizationRoutes from "./routes/organizationRoutes.js";
+import dotenv from "dotenv";
 
 // Express app instance
 const app = express();
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: "production.env" });
+} else {
+  dotenv.config({ path: "development.env" });
+}
 
 // Connect to DB
 connectDB();
 
 // CORS
 // app.use(cors(corsOptions));
-app.use(cors(
-  {
-    origin: ["https://gym-routine-creator.vercel.app", "https://gym-routine-creator.vercel.app/", "https://gym-routine-creator-drus-projects.vercel.app", "https://gym-routine-creator-git-main-drus-projects.vercel.app", "http://localhost:5173"],
+app.use(
+  cors({
+    origin: [
+      "https://gym-routine-creator.vercel.app",
+      "https://gym-routine-creator.vercel.app/",
+      "https://gym-routine-creator-drus-projects.vercel.app",
+      "https://gym-routine-creator-git-main-drus-projects.vercel.app",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
-  }
-));
+  })
+);
 
 // Routes
 app.use("/api/user", userRoutes);
