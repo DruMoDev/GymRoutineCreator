cc2c0c6ebdbd33e8dc54827280e540e1db5e963c
DruMoDev <dev.morera@gmail.com>
2024-06-27 11:34:09 +0200

    mejoras

--- Backend/config/cors.js
+++ Backend/config/cors.js
@@ -1,5 +1,5 @@
 // CORS
-let whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL, "https://gym-routine-creator.vercel.app"];
+let whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL, "https://gym-routine-creator-dru.vercel.app", "https://gym-routine-creator-dru.vercel.app/", "http://gym-routine-creator-dru.vercel.app", "http://localhost:5173"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
