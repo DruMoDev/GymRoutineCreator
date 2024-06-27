3a870c5822f7a5d2681936b738ea2d6c54ff5988
DruMoDev <dev.morera@gmail.com>
2024-06-27 11:25:03 +0200

    mejoras

--- Frontend/src/config/clienteAxios.jsx
+++ Frontend/src/config/clienteAxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-  baseURL: `https://gym-routine-creator-backend.vercel.app/api`,
-  // baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
 });
 
 export default clienteAxios;
